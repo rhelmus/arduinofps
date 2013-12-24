088c264c4fe34c3762e8ed4c36778097791a4607
Rick Helmus <rhelmus@gmail.com>
2013-12-24 23:25:56 +0100

    .

--- test5/j1firmware/render.fs
+++ test5/j1firmware/render.fs
@@ -1,7 +1,7 @@
 start-microcode render
 
 d# 10 constant STARTY
-[ RAM_SPR 2048 + ] constant RAM_SPRZOOM \ Use 2nd sprite page for zoom info
+RAM_SPRPAL constant RAM_SPRZOOM \ Use 4 256 color palettes for zoom info
 COMM+0 constant NUM_ZSPRITES \ number of sprites to zoom
 d# 4 constant SPRZOOM_SIZE \ NOTE: +1 byte to make calculating RAM_SPR pos easier
 
@@ -30,25 +30,32 @@ d# 4 constant SPRZOOM_SIZE \ NOTE: +1 byte to make calculating RAM_SPR pos easie
             \ swap ENDY < and
         until
 
-        begin
-            YLINE c@ >r
+        NUM_ZSPRITES c@ SPRZOOM_SIZE * RAM_SPRZOOM + \ end
 
-            NUM_ZSPRITES c@ SPRZOOM_SIZE * RAM_SPRZOOM + \ end
+        begin
+            \ YLINE c@ >r
+
             RAM_SPRZOOM \ start
+
             begin
                 dup 1+ @ \ yzoom
                 over c@ \ startY
 
-                r@ swap - * over c@ d# 256 * + d# 128 + d# 8 rshift \ ((yline - starty) * yzoom + (starty * 256) + 128) / 256
-                over d# 2046 - c! \ RAM_SPRZOOM - 2046 == RAM_SPR + 2 == sprite y
+                YLINE c@
+                dup d# 1 and dup>r SPR_PAGE c!
+                swap - * over c@ d# 256 * + d# 128 + d# 8 rshift \ ((yline - starty) * yzoom + (starty * 256) + 128) / 256
+                over d# 2048 r> rshift d# 2 - - c! \ RAM_SPRZOOM - (2048 >> SPR_PAGE) - 2 == RAM_SPR + 2 == sprite y
 
                 SPRZOOM_SIZE + 2dup<=
             until
 
-            2drop
-            r>
+            \ r>
+            drop
+
             VBLANK c@
         until
+
+        drop
     again
 ;
 
--- test5/test5.ino
+++ test5/test5.ino
@@ -5,21 +5,91 @@
 
 namespace {
 
+enum
+{
+    SCREEN_WIDTH = 160,
+    SCREEN_WIDTH_SPR = SCREEN_WIDTH / 16,
+    RAM_SPRZOOM = RAM_SPRPAL
+};
+
+void setPixel4bpp(uint8_t spr, uint8_t spx, uint8_t spy, uint8_t c)
+{
+    const uint16_t addr = RAM_SPRIMG + ((spr / 4) * 256) + (spy * 16) + spx;
+    uint8_t curpx = GD.rd(addr);
+    GD.wr(addr, curpx | ((c & 0b11) << (2 * (spr & 3))));
+}
+
+void setPixel16bpp(uint8_t spr, uint8_t spx, uint8_t spy, uint8_t c)
+{
+    const bool low = (spr > 63);
+    const uint16_t addr = RAM_SPRIMG + ((spr & 63) * 256) + (spy * 16) + spx;
+    uint8_t curpx = GD.rd(addr);
+    GD.wr(addr, curpx | ((c & 0b1111) << (4 * low)));
+}
+
 void initSprites(void)
 {
-    // init colours (black, red, green, blue)
     GD.wr16(PALETTE4A, RGB(255, 255, 255));
     GD.wr16(PALETTE4A + 2, RGB(255, 0, 0));
     GD.wr16(PALETTE4A + 4, RGB(0, 255, 0));
     GD.wr16(PALETTE4A + 6, RGB(0, 0, 255));
-    
+
+    GD.wr16(PALETTE4B + 0, RGB(0, 0, 255));
+    GD.wr16(PALETTE4B + 2, RGB(0, 255, 0));
+    GD.wr16(PALETTE4B + 4, RGB(255, 0, 0));
+    GD.wr16(PALETTE4B + 6, RGB(255, 255, 255));
+
+    for (uint8_t i=0; i<(2*SCREEN_WIDTH_SPR); ++i)
+    {
+        const uint8_t y = 10 + ((i / SCREEN_WIDTH_SPR) * 96);
+        GD.sprite(i, (i*16) % SCREEN_WIDTH, y, i/4, 0b1000 | ((i & 3) << 1));
+
+        for (uint8_t j=0; j<16; ++j)
+            setPixel4bpp(i, j & 15, 7, 2);
+
+        GD.wr(RAM_SPRZOOM + (i * 4), y);
+        GD.wr16(RAM_SPRZOOM + (i * 4) + 1, 256 - (256 / 5));
+    }
+
+    // Duplicate sprite page
+    for (uint16_t i=0; i<1024; ++i)
+        GD.wr(RAM_SPR + 1024 + i, GD.rd(RAM_SPR + i));
+
+    GD.wr(COMM+0, SCREEN_WIDTH_SPR);
+
+#if 0
+    GD.wr16(PALETTE16A, RGB(255, 255, 255));
+    GD.wr16(PALETTE16A + 2, RGB(255, 0, 0));
+    GD.wr16(PALETTE16A + 4, RGB(0, 255, 0));
+    GD.wr16(PALETTE16A + 6, RGB(0, 0, 255));
+
+    GD.wr16(PALETTE16B + 0, RGB(0, 0, 255));
+    GD.wr16(PALETTE16B + 2, RGB(0, 255, 0));
+    GD.wr16(PALETTE16B + 4, RGB(255, 0, 0));
+    GD.wr16(PALETTE16B + 6, RGB(255, 255, 255));
+
+    GD.sprite(0, 25, 100, 0, 0b0100);
+    GD.sprite(1, 50, 100, 1, 0b0101);
+    GD.sprite(2, 75, 100, 2, 0b1000);
+    GD.sprite(3, 100, 100, 3, 0b1001);
+
+    for (uint8_t i=0; i<16; ++i)
+    {
+        setPixel16bpp(0, i, 7, 2);
+        setPixel16bpp(1, i, 7, 2);
+        setPixel4bpp(2, i, 7, 2);
+        setPixel4bpp(3, i, 7, 2);
+    }
+#endif
+
+#if 0
     for (uint8_t i=0; i<16; ++i)
         GD.fill(RAM_SPRIMG + (i*16), (i / 4), 16);
 
     uint16_t spr = 0;
     for (uint16_t y=16; y<200; y+=32)
     {
-        for (uint16_t x=0; x<250; x+=32)
+        for (uint16_t x=0; x<350; x+=32)
         {
             GD.sprite(spr, x, 0, 0, 0b1000);
             GD.wr(RAM_SPR + 2048 + (spr * 4), y);
@@ -30,6 +100,7 @@ void initSprites(void)
     }
 
     GD.wr(COMM+0, spr);
+#endif
 
 #if 0
     // ref sprites
@@ -37,7 +108,7 @@ void initSprites(void)
         GD.sprite(spr++, 128, y, 0, 0b1000);
 #endif
 }
-    
+
 }
 
 
